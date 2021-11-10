e6be202a8275c80329052a8c5f681cda3de64e3e
Ajahar Vishnu <vishnu.ajahar@gmail.com>
2021-11-10 23:52:12 +0530

    Added random background functionality

--- app.js
+++ app.js
@@ -10,3 +10,13 @@ btn.addEventListener("click", function () {
   console.log(totalBill);
   document.getElementById("total-bill").value = totalBill;
 });
+
+let clicker = document.querySelector("#container");
+clicker.addEventListener("click", function () {
+  console.log("hello");
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  let randomColor = `rgb(${r}, ${g}, ${b})`;
+  document.body.style.backgroundColor = randomColor;
+});
